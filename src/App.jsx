56bcb726a1785112c58a77d0b7f3a9e0623b99a1
liz-garcia <lizgarciamarin@gmail.com>
2024-01-12 21:22:54 +0100

    Installed prettier.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Title hey there! React App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
