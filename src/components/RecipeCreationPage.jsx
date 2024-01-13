e649872cbdb74446406bf5037de48aab27f46e5d
liz-garcia <lizgarciamarin@gmail.com>
2024-01-13 13:00:43 +0100

    Fixed issue with no-unescaped-entities eslint-plugin-react in RecipeCreationPage.jsx.

--- src/components/RecipeCreationPage.jsx
+++ src/components/RecipeCreationPage.jsx
@@ -7,7 +7,7 @@ const RecipeCreationPage = () => {
   return (
     <>
       <h1>{userContext.userName ? `Good to see you, ${userContext.userName}!` : 'Good to see you!'}</h1>
-      <h2>Let's choose some ingredients.</h2>
+      <h2>Let&apos;s choose some ingredients.</h2>
     </>
   );
 };
