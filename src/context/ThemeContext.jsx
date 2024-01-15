841c2af23a74ff1cc2398569ed7e08f999af5008
liz-garcia <lizgarciamarin@gmail.com>
2024-01-15 17:22:37 +0100

    Modified ThemeContext in order to also update the theme based on system color scheme.

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,5 +1,4 @@
-// ThemeContext.js
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const ThemeContext = createContext();
@@ -10,6 +9,30 @@ const ThemeContextProvider = ({ children }) => {
   const toggleTheme = () => {
     setIsDarkMode((prevMode) => !prevMode);
   };
+
+  useEffect(() => {
+    // Function to update the theme based on system color scheme
+    const updateTheme = () => {
+      const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+      setIsDarkMode(prefersDarkMode);
+    };
+
+    // Call the function on component mount
+    updateTheme();
+
+    // Listen for changes in system color scheme
+    const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleChange = () => {
+      updateTheme();
+    };
+
+    mediaQueryList.addEventListener('change', handleChange);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      mediaQueryList.removeEventListener('change', handleChange);
+    };
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
